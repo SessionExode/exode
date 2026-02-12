15954f25cc580da9503a599e963ada7b2403dfd6
Vianney HERVY <vian.hervy@gmail.com>
2026-02-12 19:04:59 +0100

    Added french translation template

--- exode.php
+++ exode.php
@@ -3,6 +3,9 @@
 /*
  * Plugin Name: Exode
  * Plugin URI: https://github.com/SessionExode/exode
+ * Text DOmain: exode
+ * Domain Path: /lang
+ *
  * Description: Main plugin of the Session Exode App
  * Author: Thibault Chourré & Vianney Hervy
  */
@@ -10,6 +13,8 @@
 if (!defined("ABSPATH")) {
     exit;
 }
+
+add_action("init", fn() => load_plugin_textdomain("exode", false, dirname(plugin_basename(__FILE__)) . "/lang"));
 
 // Load Composer Autoloader
 if (file_exists(__DIR__ . "/vendor/autoload.php")) {
